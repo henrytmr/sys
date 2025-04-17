c0644d9803957f862b2535a5ec1849d2a5ebe5e4
henrytmr <henryruztamayo@gmail.com>
2025-04-16 22:59:06 -0400

    XFSOCIY

---
+++ conf.py
@@ -0,0 +1,6 @@
+import os
+
+api_id = os.environ.get('29752600')
+api_hash = os.environ.get('4840ecd4ccc76181dfcb09df79b4a624')
+bot_token = os.environ.get('7339484326:AAGeDLwo6By07N6AUnsWlHV6DinyhxORzSY')
+bot_admin_group = os.environ.get('rcbot')
